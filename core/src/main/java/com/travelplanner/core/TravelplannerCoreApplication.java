103730ef94fbf7eaee0791f226c2afba3c5f97c9
Antonio Cesar Urbano Da Silv Filho <cesar@Antonios-MacBook-Air.local>
2025-04-15 00:10:23 -0300

    projeto inicial'

---
+++ core/src/main/java/com/travelplanner/core/TravelplannerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.travelplanner.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelplannerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelplannerCoreApplication.class, args);
+	}
+
+}
